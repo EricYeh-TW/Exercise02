3b93257b62098f835c32cba837675512fbbd9834
Eric Yeh <spu8673@gmail.com>
2022-02-04 02:24:46 +0800

    exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation.js";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition.js";
+import Logo from "./components/Logo/Logo.js";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm.js";
+import Rank from "./components/Rank/Rank.js";
+import Clarifai from 'clarifai';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const app = new Clarifai.App({
+  apiKey: '271ca4335e4e49e483ecd4fd2e591750'
+ });
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonClick = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        console.log(err);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange = {this.onInputChange}
+          onButtonClick = {this.onButtonClick}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Tilt from "react-tilt";
+import icon from './icon.png';
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0 ">
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 10 }} style={{ height: 150, width: 150, backgroundColor: "lightblue"}} >
+        <div className="Tilt-inner pa3">
+          <img src={icon} alt="logo" />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
