775dae52b74b7a02ab20dda2a71e6ee070b9c2ff
Eric Yeh <spu8673@gmail.com>
2022-02-18 19:00:22 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -79,9 +79,7 @@ class App extends Component {
           })
             .then(res => res.json())
             .then(count => {
-              this.setState({user: {
-                entries: count
-              }})
+              this.setState(Object.assign(this.state.user, { entries: count }))
             })
         }
         this.detectBox(this.faceLocation(response))
